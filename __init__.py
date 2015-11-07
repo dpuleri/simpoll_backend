d956526708782485791a01599fff3a24148a58ca
Daniel Puleri <daniel.puleri@gmail.com>
2015-11-07 01:19:16 -0500

    Add precursor routes

--- __init__.py
+++ __init__.py
@@ -1,6 +1,24 @@
 from flask import Flask
-app = Flask(__name__)
+from flask.ext.mongoengine import MongoEngine
+from Simpoll import restful
 
+app = Flask(__name__)
+app.config["MONGODB_SETTINGS"] = {'DB': "simpoll_db"}
+app.config["SECRET_KEY"] = "this_is_only_a_test"
+
+db = MongoEngine(app)
+
+@app.route("/")
+def hello():
+    return "Hello this is world"
+
+@app.route('/<poll_id>', methods=['GET'])
+def api_poll(poll_id):
+    return restful.get_poll(poll_id)
+
+@app.route('/polls', methods=['GET'])
+def api_polls():
+    return restful.get_polls()
 
 if __name__ == '__main__':
     app.run()
